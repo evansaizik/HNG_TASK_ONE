56d0fcb857ccc14aecbb1133d203d2eb55f4ae5c
isaiah usiholo <evansaizik@gmail.com>
2022-10-27 14:07:37 +0100

    python books update

--- src/components/Body/Link.js
+++ src/components/Body/Link.js
@@ -8,7 +8,7 @@ const Links = () => {
       <Button id='Links'><a href="https://twitter.com/evansaizik">Twitter</a></Button>
       <Button id='btn__zuri'><a href="https://training.zuri.team/">Zuri Team</a></Button>
       <Button id='books'><a href="http://books.zuri.team">Zuri Books</a></Button>
-      <Button id='book__python'><a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">Python Books</a></Button>
+      <Button id='book__python'><a href="https://books.zuri.team/python-for-beginners?ref_id=Evans_Isaiah">Python Books</a></Button>
       <Button id='pitch'><a href="https://background.zuri.team">Background Check for Coders</a></Button>
       <Button id='book__design'><a href="https://books.zuri.team/design-rules">Design Books</a></Button>
     </div>
