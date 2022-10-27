39c17652c869540fa5b3e0c6b0d65fce0c0c988c
isaiah usiholo <evansaizik@gmail.com>
2022-10-27 13:16:56 +0100

    web layout design and page modification

--- src/components/Body/Profile.js
+++ src/components/Body/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './Profile.module.css';
 import share from '../../Assets/shareLogo.svg';
+import share1 from '../../Assets/shareLogo1.svg';
 
 const Profile = () => {
   return(
@@ -9,6 +10,7 @@ const Profile = () => {
       >
       <img src="https://avatars.githubusercontent.com/u/108002096?v=4" id="profile__img" alt="profile_photo"/>
       <img className={classes.img} src={share} alt='share'/>
+      <img className={classes.img768} src={share1} alt='share'/>
       <h1 id="twitter">evansaizik</h1>
       <h1 id="slack">Evans Isaiah</h1>
     </div>
