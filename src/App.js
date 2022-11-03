e6eee3845ceb1e13fa3e3c90b1f141660242010d
isaiah usiholo <evansaizik@gmail.com>
2022-11-03 18:42:26 +0100

    docs: installed, imported and implemented  my router dependecies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Container from "./components/Body/Container";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route } from 'react-router-dom';
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div>
-      <Container />
+      <Routes>
+        <Route path="/" element={<Container />}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
       <Footer />
     </div>
   );
