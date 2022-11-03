b648a4d455b92a56e7b5af4d47afde2f83d3bfe5
isaiah usiholo <evansaizik@gmail.com>
2022-11-03 21:26:21 +0100

    style: modification of the Footer and contact form web display

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,21 +8,23 @@ const Contact = () => {
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
     </header>
     <form>
-      <div>
-        <label htmlFor="first_name">First name</label>
-        <input id="first_name"/>
+      <div className={classes.name}>
+        <div className={classes.row}>
+          <label htmlFor="first_name">First name</label>
+          <input placeholder="Enter your first name" id="first_name"/>
+        </div>
+        <div className={classes.row}>
+          <label htmlFor="last_name">Last name</label>
+          <input placeholder="Enter your last name" id="last_name"/>
+        </div>
       </div>
-      <div>
-        <label htmlFor="last_name">Last name</label>
-        <input id="last_name"/>
-      </div>
-      <div>
+      <div className={classes.email}>
         <label htmlFor="email">Email</label>
-        <input id="email"/>
+        <input placeholder="yourname@email.com" id="email"/>
       </div>
       <div className={classes.text}>
         <label htmlFor="message">Message</label>
-        <textarea id="message"/>
+        <textarea placeholder="Send me a message and I'll reply you as soon as possible." id="message"/>
       </div>
       <div className={classes.check}>
         <input id="checkbox" type="checkbox"/>
